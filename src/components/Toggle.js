55ab269f3a2ec79854491065fc29a9444336901b
Samson Githinji <githinjisamson148@gmail.com>
2023-10-27 06:46:54 +0300

    Completed assignment

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  // maintain state
+  const [isOn, setIsOn] = useState(false);
+
+  // eventHandler/avoid invoking immediately
+  const handleBtnClick = () => {
+    setIsOn((prevState) => {
+      // toggle state
+      const newState = !prevState;
+      return newState;
+    });
+  };
+
+  // determine color
+  const color = isOn ? "red" : "white";
+
+  // style object
+  const btnBackground = {
+    backgroundColor: color,
+  };
+
+  return (
+    <button onClick={handleBtnClick} style={btnBackground}>
+      {/* conditional rendering */}
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
